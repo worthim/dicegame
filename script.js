9dc9d24883f29f374de1e346d3825a045c876c41
CN77 <cn77@CN77s-MacBook-Pro.local>
2019-09-10 11:45:11 +0100

    completed single player dice game

---
+++ script.js
@@ -0,0 +1,31 @@
+const button=document.getElementById("rollButton")
+let totalScore = 0;
+
+const diceImage=document.getElementById("dice")
+
+const score = document.getElementById("score")
+const title = document.getElementById("title1")
+
+
+
+
+button.addEventListener("click",()=> {
+    const randomNumber= Math.floor(Math.random()*6+1)
+    console.log(randomNumber)
+    totalScore=totalScore+randomNumber
+    diceImage.src=`/img/dice${randomNumber}.png`
+    score.textContent = totalScore
+    title.textContent = "The Dice Game !"
+
+    if (totalScore>=21) {
+        title.textContent="You are Winner !"
+        totalScore=0
+
+    }
+
+    if (randomNumber==1) {
+        title.textContent="You lost!"
+        totalScore=0
+    }
+})
+
